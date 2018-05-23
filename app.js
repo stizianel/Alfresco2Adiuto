7024584cbb2dd6fdc39a08e3e62f78b2b705479c
stizianel <stizianel@gmail.com>
2018-05-23 17:44:22 +0200

    stabilizzata al 23.05.2018

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 const parseXml = require('./lib/parse-xml.js');
+const ws = require('./lib/adiutoWs.js');
 
 
 const xml = fs.readFileSync(`${__dirname}/test/testpk09.xml`);
 
 let result = parseXml(xml);
 
-result.map((r) => {
-    console.log(JSON.stringify({ index: { _id: `pg${r.contentName.match(/[0-9]+/g)}` } }));
-    console.log(JSON.stringify(r, null, ' '));
-});
+// result.map((r) => {
+//     console.log(JSON.stringify({ index: { _id: `pg${r.contentName.match(/[0-9]+/g)}` } }));
+//     console.log(JSON.stringify(r));
+// });
 
+// const adiuto = new ws();
+// let session = adiuto.connect();
+// console.log('app ' + session);
 
-//console.log(JSON.stringify(result, null, ' '));
+console.log(JSON.stringify(result, null, ' '));
+
 
 
